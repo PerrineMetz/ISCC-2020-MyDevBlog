421905489e21c076c7b699205abe0415597c3b14
PerrineMetz <67265994+PerrineMetz@users.noreply.github.com>
2020-07-15 19:30:51 +0200

    my DevBlog ...

---
+++ HTMLPage1.php
@@ -0,0 +1,51 @@
+﻿<!DOCTYPE html>
+
+<link rel="stylesheet" type="text/css" href="index.css" media="screen" />
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
+
+<head>
+    <meta charset="utf-8" />
+    <title>Perrine Metz's DevBlog</title>
+</head>
+
+<body>
+    <div id="preloader">
+        <div class="load-wrapp">
+            <div class="load-10">
+                <p>Devblog experience</p>
+                <p class="txtloading">By Perrine Metz.</p>
+                <div class="bar"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="w3-bar w3-black w3-hide-small">
+        <a href="#" class="w3-bar-item w3-button w3-right">Connexion</i></a>
+        <a href="#" class="w3-bar-item w3-button w3-right">Accueil</i></a>
+        <a href="#" class="w3-bar-item w3-button w3-right">Articles</i></a>
+        <a href="contact.html" class="w3-bar-item w3-button w3-right">Contact</i></a>
+    </div>
+
+    <div class="w3-bar w3-black w3-bottom w3-hide-small">
+        <a href="#" class="w3-bar-item w3-button"><i class="fa fa-instagram"></i></a>
+        <a href="#" class="w3-bar-item w3-button"><i class="fa fa-linkedin"></i></a>
+        <a href="#" class="w3-bar-item w3-button w3-right">perrine.metz@jesaispas.com</i></a>
+    </div>
+
+    <section>
+        <div class="container">
+            <p class="desc1">Hi,<br/>Welcome to my DevBlog experience.</p>
+            <a class="txt">By Perrine Metz.</a>
+            <p class="desc2">Bienvenue sur mon DevBlog. Je m’appelle Perrine et je suis étudiante en première année à l’ISEG à Strasbourg. Pendant 4 semaines j’ai appris grâce au Summer Code Camp à utiliser des notions de programmation. Je me suis ouverte à un nouveau domaine, chose que je n’aurai peut-être pas fait si cette formation ne m’avait pas été proposée. J’ai développé ce blog afin de vous partager quelques-unes des notions que j’ai pu apprendre.</p>
+        </div>
+    </section>
+
+    <script src="index.js"></script>
+</body>
+</html>
---
+++ connexion.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+    <link rel="stylesheet" type="text/css" href="index.css" media="screen" />
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    
+    <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+        <title>Contact</title>
+    </head>
+
+<body>
+    
+    <div id="preloader">
+        <div class="load-wrapp">
+            <div class="load-10">
+                <p>Devblog experience</p>
+                <p class="txtloading">By Perrine Metz.</p>
+                <div class="bar"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="w3-bar w3-black w3-hide-small">
+        <a href="#" class="w3-bar-item w3-button w3-right">Connexion</i></a>
+        <a href="HTMLPage1.html" class="w3-bar-item w3-button w3-right">Accueil</i></a>
+        <a href="#" class="w3-bar-item w3-button w3-right">Articles</i></a>
+        <a href="#" class="w3-bar-item w3-button w3-right">Contact</i></a>
+    </div>
+
+    <div class="w3-bar w3-black w3-bottom w3-hide-small">
+        <a href="#" class="w3-bar-item w3-button"><i class="fa fa-instagram"></i></a>
+        <a href="#" class="w3-bar-item w3-button"><i class="fa fa-linkedin"></i></a>
+        <a href="#" class="w3-bar-item w3-button w3-right">perrine.metz@jesaispas.com</i></a>
+    </div>
+
+    <?php
+    session_start();
+    ?>
+        
+    <form action="securite.php" method="post">
+        <input type="text" placeholder="Login" name="Login">
+            <br>
+        
+            <input type="text" placeholder="Password" name="password">
+            <br>
+            <button type="submit" value="Envoyer">Envoyer</button>
+        </form>
+        </body>
+    </html>
+
+    <footer></footer>
+    <script src="index.js"></script>
+</body>        
+</html>
